7f3fd80b4051922b5aa2482d904689071ea138b7
Kimjunghyunn <godqhr6846@daum.net>
2022-09-22 21:59:14 +0900

    프로그래머스 레벨1: 문자열을 정수로 바꾸기

---
+++ src/StringInt.java
@@ -0,0 +1,9 @@
+public class StringInt {
+    public static void main(String[] args) {
+        System.out.println(solution("1234"));
+        System.out.println(solution("-1234"));
+    }
+    static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
